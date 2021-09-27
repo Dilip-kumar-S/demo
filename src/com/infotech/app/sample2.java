dabab0809174eb128379ee9a54332a83b2d6da2e
dilip <dilip958895@gmail.com>
2021-09-27 21:06:43 +0530

    added find name method

--- src/com/infotech/app/sample2.java
+++ src/com/infotech/app/sample2.java
@@ -2,8 +2,16 @@ package com.infotech.app;
 
 public class sample2
 {
+	
 public static void main(String[] args) 
 {
+
+	new sample2().findName("dilip");
 	System.out.println("Sample 2 class is added");
+}
+
+public String findName(String name)
+{
+	return name;
 }
 }
